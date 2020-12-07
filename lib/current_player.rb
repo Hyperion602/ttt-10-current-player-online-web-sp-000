af7d26dd169e8d9100b835e6ccbc25b5f8b706ba
James Harris <mharris9608@yahoo.com>
2020-12-07 20:10:39 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -7,3 +7,12 @@ def turn_count(board)
   end
   return counter
 end
+
+def current_player(board)
+  if turn_count % 2 = 0
+    return "X"
+  elsif turn_count % 2 != 0
+    return "O"
+  else
+  end
+end
