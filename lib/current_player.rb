4be732646a231ff476fd3645cf37611741b31f2f
James Harris <mharris9608@yahoo.com>
2020-12-07 19:20:54 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,3 @@
 def turn_count(board)
   counter = 0
-  board.each do |position|
-    if position == "X" || "O"
-      counter += 1
-      return counter
-    end
-  end
-end
+  board.each{|position| == "X" || "O" counter += 1}
